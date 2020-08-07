c886205450416255cc0cbe3fe4cc2f50b9cfbf42
jean <jeancs.patricio@gmail.com>
2020-08-07 16:38:30 -0300

    Refactoring author label string.

--- app/src/main/java/com/jeancsanchez/reddit/PostAdapter.kt
+++ app/src/main/java/com/jeancsanchez/reddit/PostAdapter.kt
@@ -7,8 +7,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 class PostAdapter : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
     var postList: List<Post> = emptyList()
@@ -35,29 +35,16 @@ class PostAdapter : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
         private val imgPlayVideo = view.rootView.findViewById<ImageView>(R.id.imgPlayVideoPost)
 
         fun bind(post: Post) {
-            val today = Date()
-            val difference = today.time - post.createdAt.time
-            val seconds = TimeUnit.MILLISECONDS.convert(difference, TimeUnit.SECONDS)
-            val minutes = TimeUnit.SECONDS.convert(seconds, TimeUnit.MINUTES)
-            val hours = TimeUnit.MINUTES.convert(seconds, TimeUnit.HOURS)
-            val days = TimeUnit.HOURS.convert(hours, TimeUnit.DAYS)
-            var datePeriod = "$days days"
-
-            if (days < 1) {
-                if (hours < 60) {
-                    datePeriod = "$hours hours"
-                    if (minutes < 60) {
-                        datePeriod = "$minutes minutes"
-                        if (seconds < 60) {
-                            datePeriod = "$seconds seconds"
-                        }
-                    }
-                }
-            }
-
-            txtAuthor.text =
-                txtAuthor.context.getString(R.string.post_author, post.author, datePeriod)
             txtTitle.text = post.title
+
+            txtAuthor.apply {
+                val dateFormat = SimpleDateFormat("dd/MM/yy 'às' HH:MM", Locale.getDefault())
+                text = context.getString(
+                    R.string.label_post_author,
+                    post.author,
+                    dateFormat.format(post.createdAt)
+                )
+            }
 
             if (post.hasMedia) {
                 imgPost.visibility = View.VISIBLE
